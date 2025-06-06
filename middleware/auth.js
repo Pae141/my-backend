7e53838bc1c8fb985fc23bad0e31476d5220f4d5
Pae141 <attapon141@gmail.com>
2025-06-06 21:25:02 +0700

    Update server.js and environment variables

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // ใช้จาก env
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = authenticate;
