62eea7536e1b3854a0292f454c8015b42892e1e8
Pae141 <attapon141@gmail.com>
2025-06-06 22:29:38 +0700

    Update server.js and environment variables

--- db.js
+++ db.js
@@ -2,13 +2,12 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   ssl: { rejectUnauthorized: false },
-  // บังคับใช้ IPv4
-  // ในบางกรณีต้องใส่ connectionTimeoutMillis เพื่อช่วย
-  connectionTimeoutMillis: 5000,
-  keepAlive: true,
-  family: 4,
 });
 
 module.exports = pool;
