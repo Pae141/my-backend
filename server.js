995cf8dd246d9f4fbf59938bdc982b148978d3c0
Pae141 <attapon141@gmail.com>
2025-06-07 14:00:55 +0700

    Update server.js and environment variables

--- server.js
+++ server.js
@@ -18,8 +18,13 @@ const app = express();
 const allowedOrigins = ['http://localhost:3000','http://localhost:5173', 'https://pae141.github.io'];
 
 app.use(cors({
-  origin: allowedOrigins,     // ✅ แบบ array จะกำหนด header ได้ถูกต้อง
-  credentials: true,          // ✅ ให้ cookie ส่งข้าม origin ได้
+  origin: allowedOrigins,
+  credentials: true,
+}));
+
+app.options('*', cors({
+  origin: allowedOrigins,
+  credentials: true,
 }));
 
 app.use(cookieParser());
